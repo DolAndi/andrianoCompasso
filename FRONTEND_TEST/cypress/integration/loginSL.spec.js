61d7ee816a7ddf2f4566bbff0273498ac4abae6d
Andriano <anditoazza@gmail.com>
2021-10-27 11:29:44 -0300

    integração de describe

--- FRONTEND_TEST/cypress/integration/loginSL.spec.js
+++ FRONTEND_TEST/cypress/integration/loginSL.spec.js
@@ -6,13 +6,17 @@ describe("Teste de Login SwagLab", () =>{
     beforeEach(() =>{
         cy.visit("https://www.saucedemo.com")
     })
-    it("Deve efetuar o login correto", ()=>{
-        cy.login()
+    describe("Teste de Login SwagLab COM sucesso", () =>{
+        it("Deve efetuar o login correto", ()=>{
+            cy.login()
+        })
     })
-    it("Deve efetuar o login com usuário incorreto", ()=>{
-        cy.loginUsuarioErrado()
-    })
-    it("Deve efetuar o login com senha incorreta", ()=>{
-        cy.loginSenhaErrada()
+    describe("Teste de Login SwagLab SEM sucesso", () =>{
+        it("Deve efetuar o login com usuário incorreto", ()=>{
+            cy.loginUsuarioErrado()
+        })
+        it("Deve efetuar o login com senha incorreta", ()=>{
+            cy.loginSenhaErrada()
+        })
     })
 }) 
